4a1f086e246242e528880a4fd516c8522b991223
Reza Shams <rezashams86@gmail.com>
2021-10-30 09:39:55 +0100

    Add User service

---
+++ src/test/java/com/hotel/web/HotelAppGuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAppGuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
